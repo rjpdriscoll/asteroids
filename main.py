665974d7a48fd7e76a032535716198cb888c7294
rjpdriscoll <rjpdriscoll@gmail.com>
2025-12-16 14:12:43 -0700

    shape drawn to screen

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 
 def main():
@@ -15,7 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
